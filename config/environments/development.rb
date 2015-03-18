5c6ffce66ed90d6776670f70d43cd46765b93f63
Elias Wehbe <elias.t.wehbe@gmail.com>
2015-03-17 22:23:21 -0400

    final changes to development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,5 @@ Pinteresting::Application.configure do
   config.assets.debug = true
 
   #Required for Heroku
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
